3c496d6282151de7b8125b3e40076d0e55b1edf9
Chris DeMars <cdemars86@gmail.com>
2020-05-09 21:32:11 -0600

    more shit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
+import FooterStyles from '../styles/footer.module.scss';
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="copyright">
-        &copy; Chris DeMars 2020
-      </div>
-      <div className="social-links">
-        <ul>
-          <li href=""></li>
-          <li href=""></li>
-        </ul>
+    <footer className={FooterStyles.footer}>
+      <div className={FooterStyles.footerWrapper}>
+        <div className="copyright">&copy; Chris DeMars 2020</div>
+        <div className="social-links">
+          <ul>
+            <li>LinkedIn</li>
+            <li>Twitter</li>
+          </ul>
+        </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
-
+export default Footer;
--- src/components/hire.js
+++ src/components/hire.js
@@ -1,16 +1,21 @@
-import React from "react"
+import React from 'react';
+import HireStyles from '../styles/hire.module.scss';
 
 const Hire = () => {
   return (
-    <section>
-      <h2>Hire Me!</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, mollitia
-        natus expedita hic nostrum aliquid? Nisi excepturi neque eos vitae.
-      </p>
-      <button>Get in touch!</button>
+    <section className={HireStyles.hireSection}>
+      <div className={HireStyles.hireWrapper}>
+        <h2>Hire Me!</h2>
+        <p>
+          Are you looking for an accessibility specialist to create an
+          experience that is usable for everyone? What about someone that can
+          architect the user experience? Maybe a developer with amazing CSS
+          chops? Lets work together. I'd love to see what you having going on!
+        </p>
+        <button className={HireStyles.btn}>Get in touch!</button>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hire
+export default Hire;
